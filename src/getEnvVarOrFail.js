910f62622326ac6a15068f8431917e0b468727b5
nikelborm <kolya007.klass@gmail.com>
2023-11-03 09:27:10 +0300

    init

---
+++ src/getEnvVarOrFail.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} name
+ */
+export function getEnvVarOrFail(name) {
+  const envVar = process.env[name];
+
+  if (!envVar)
+    throw new Error(`env var ${name} is not defined`);
+
+  return envVar;
+}
