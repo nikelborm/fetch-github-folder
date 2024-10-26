3e9e374d9a33a33940416c08ac991cc9bb5f1e3f
nikelborm <29357907+nikelborm@users.noreply.github.com>
2024-10-26 22:37:53 +0300

    save

--- index.ts
+++ index.ts
@@ -1,10 +1,7 @@
-// @ts-check
-'use strict';
-
 import {
   downloadDirectoryRecursively,
   getEnvVarOrFail,
-} from "./src/index.js";
+} from "./src";
 
 await downloadDirectoryRecursively({
   githubAccessToken: getEnvVarOrFail('GITHUB_ACCESS_TOKEN'),
@@ -13,5 +10,5 @@ await downloadDirectoryRecursively({
     name: getEnvVarOrFail('GITHUB_REPO_NAME'),
   },
   pathToDirectoryInRepo: getEnvVarOrFail('PATH_TO_DIRECTORY_IN_REPO'),
-  commitShaHashOrBranchNameOrTagName: process.env.COMMIT_SHA_HASH_OR_BRANCH_NAME_OR_TAG_NAME,
+  commitShaHashOrBranchNameOrTagName: process.env['COMMIT_SHA_HASH_OR_BRANCH_NAME_OR_TAG_NAME'],
 });
--- src/index.js
+++
@@ -1,7 +0,0 @@
-// @ts-check
-'use strict';
-
-export { downloadDirectoryContentsMetaInfo } from './downloadDirectoryContentsMetaInfo.js';
-export { downloadDirectoryRecursively } from './downloadDirectoryRecursively.js';
-export { downloadInfoAboutAllBlobsInDirectory } from './downloadInfoAboutAllBlobsInDirectory.js';
-export { getEnvVarOrFail } from './getEnvVarOrFail.js';
