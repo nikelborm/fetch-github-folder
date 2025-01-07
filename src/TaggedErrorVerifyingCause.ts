8b17c6054406667c1cc00a4f43457abe0b10fc1a
nikelborm <evadev@duck.com>
2025-01-07 22:41:40 +0300

    quick save: Tue Jan  7 22:41:40 MSK 2025

--- src/TaggedErrorVerifyingCause.ts
+++ src/TaggedErrorVerifyingCause.ts
@@ -1,34 +1,42 @@
+import { YieldableError } from 'effect/Cause';
 import { TaggedError } from 'effect/Data';
+import { Equals } from 'effect/Types';
 
 export const TaggedErrorVerifyingCause = <
-  const ErrorName extends string,
-  ExpectedCauseClass extends new(...args: any[]) => Error
+  Context extends Record<string, any> = {}
+>() => <
+  const ErrorName extends string = string,
+  ExpectedCauseClass extends new(...args: any[]) => Error = new(...args: any[]) => Error,
+  ContextArg = Equals<Context, {}> extends true ? void : Context,
 >(
   errorName: ErrorName,
   expectedCauseClass: ExpectedCauseClass,
   customMessage: string
 ): {
-  new (cause: InstanceType<ExpectedCauseClass>): (
-    InstanceType<typeof CustomTaggedErrorClass>
-    & { name: ErrorName }
-  )
+  new (
+    cause: InstanceType<ExpectedCauseClass>,
+    ctx: ContextArg
+  ): YieldableError & Readonly<{
+    message: string;
+    _tag: ErrorName;
+    name: ErrorName;
+    cause: InstanceType<ExpectedCauseClass>;
+  } & Context>
 } => {
   const CustomTaggedErrorClass = TaggedError(errorName)<{
-    message: string,
-    name: ErrorName,
-    cause: InstanceType<ExpectedCauseClass>
+    message: string
   }>;
 
   class Base extends CustomTaggedErrorClass {
-    constructor(cause: InstanceType<ExpectedCauseClass>) {
+    constructor(cause: unknown, ctx: ContextArg) {
       if (!(cause instanceof expectedCauseClass))
         throw new Error(`Provided cause of incorrect type to ${
           errorName
         } class. Expected cause class: "${expectedCauseClass.name}"`);
 
-      super({ message: customMessage, cause, name: errorName });
+      super({ message: customMessage, cause, name: errorName, ...ctx });
     }
   }
 
-  return Base;
+  return Base as any;
 }
--- src/errors.ts
+++ src/errors.ts
@@ -1,43 +1,43 @@
 import { RequestError } from '@octokit/request-error';
 import { TaggedErrorVerifyingCause } from './TaggedErrorVerifyingCause.js';
 
-export class GitHubApiGeneralServerError extends TaggedErrorVerifyingCause(
+export class GitHubApiGeneralServerError extends TaggedErrorVerifyingCause()(
   'GitHubApiGeneralServerError',
   RequestError,
   'GitHub API Error: Bad server'
 ) {}
 
-export class GitHubApiGeneralUserError extends TaggedErrorVerifyingCause(
+export class GitHubApiGeneralUserError extends TaggedErrorVerifyingCause()(
   'GitHubApiGeneralUserError',
   RequestError,
   'GitHub API Error: Bad user, invalid request'
 ) {}
 
-export class GitHubApiRepoDoesNotExistsOrPermissionsInsufficient extends TaggedErrorVerifyingCause(
+export class GitHubApiRepoDoesNotExistsOrPermissionsInsufficient extends TaggedErrorVerifyingCause()(
   'GitHubApiRepoDoesNotExistsOrPermissionsInsufficient',
   RequestError,
   'GitHub API Error: Repo does not exists or you do not have permissions to access it'
 ) {}
 
-export class GitHubApiRepoIsEmpty extends TaggedErrorVerifyingCause(
+export class GitHubApiRepoIsEmpty extends TaggedErrorVerifyingCause()(
   'GitHubApiRepoIsEmpty',
   RequestError,
   'GitHub API Error: This Repo is empty'
 ) {}
 
-export class GitHubApiBadCredentials extends TaggedErrorVerifyingCause(
+export class GitHubApiBadCredentials extends TaggedErrorVerifyingCause()(
   'GitHubApiBadCredentials',
   RequestError,
   'GitHub API Error: Token you\'re using is invalid.'
 ) {}
 
-export class GitHubApiAuthRatelimited extends TaggedErrorVerifyingCause(
+export class GitHubApiAuthRatelimited extends TaggedErrorVerifyingCause()(
   'GitHubApiAuthRatelimited',
   RequestError,
   'GitHub API Error: Too many invalid auth attempts. Chillout pal'
 ) {}
 
-export class GitHubApiRatelimited extends TaggedErrorVerifyingCause(
+export class GitHubApiRatelimited extends TaggedErrorVerifyingCause()(
   'GitHubApiRatelimited',
   RequestError,
   'GitHub API Error: Too many requests. Chillout pal'
