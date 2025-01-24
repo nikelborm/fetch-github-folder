37e104ad201a36ddff0ded02ad1fafe852ed42ab
nikelborm <evadev@duck.com>
2025-01-24 08:43:45 +0000

    Auto-updated NPM package files

--- fetch-github-folder.ts
+++ fetch-github-folder.ts
@@ -20,7 +20,7 @@ import {
 
 // Those values updated automatically. If you edit names of constants or
 // move them to a different file, update ./scripts/build.sh
-const PACKAGE_VERSION = '0.1.18';
+const PACKAGE_VERSION = '0.1.19';
 const PACKAGE_NAME = 'fetch-github-folder';
 
 const appCommand = make(
