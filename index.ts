c3155fc9413205fd62396d2dee059f94ae28c385
nikelborm <evadev@duck.com>
2025-01-24 04:07:35 +0300

    quick save: Fri Jan 24 04:07:35 MSK 2025

--- index.ts
+++ index.ts
@@ -5,6 +5,7 @@
 export { downloadEntityFromRepo } from './src/downloadEntityFromRepo.js';
 export { OctokitLayer } from './src/octokit.js';
 export * from './src/repo.interface.js';
+export { SingleTargetConfig } from './src/configContext.js';
 
 export * from './errors.js';
 export * from './cli.js';
