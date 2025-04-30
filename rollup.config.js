f0ca995d4bc546c304a73c97d81f1c4e8cf268ec
nikelborm <evadev@duck.com>
2025-04-30 03:10:31 +0300

    quick save: Wed Apr 30 03:10:31 MSK 2025

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import json from '@rollup/plugin-json';
 import { visualizer } from 'rollup-plugin-visualizer';
 import terser from '@rollup/plugin-terser';
 import commonjs from '@rollup/plugin-commonjs';
@@ -14,6 +15,7 @@ export default {
   plugins: [
     nodeResolve(),
     commonjs(),
+    json(),
     visualizer({
       sourcemap: true,
       filename: 'gh-page/bundled_deps/index.html',
